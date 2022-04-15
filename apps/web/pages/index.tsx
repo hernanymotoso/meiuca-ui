388511c975c3abc8cbc24f80f1fe682f869b9e4e
Hernany Motoso <contatohernany@gmail.com>
2022-04-15 10:42:40 -0300

    feat: initial setup finished and the color pack creation

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "ui";
+import { BaseColors } from 'colors';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 style={{ backgroundColor: `${BaseColors["color-primary-02"]}` }}>Web</h1>
       <Button />
     </div>
   );
---
+++ packages/colors/src/index.ts
@@ -0,0 +1,15 @@
+const BaseColors = {
+    'color-primary-01': '#691C07', // Brand colors
+    'color-primary-02': '#8F260A',
+    'color-primary-03': '#F04E23',
+    'color-primary-04': '#FBD1C6',
+    'color-primary-05': '#FEF0EC',
+    'color-neutral-01': '#000000', // Neutral colors
+    'color-neutral-02': '#70747B',
+    'color-neutral-03': '#A6AAAF',
+    'color-neutral-04': '#E4E5E7',
+    'color-neutral-05': '#FFFFFF'
+}
+
+
+export { BaseColors }
