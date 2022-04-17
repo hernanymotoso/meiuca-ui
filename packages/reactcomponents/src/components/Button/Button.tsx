3b4455a477305ec6ca9fac041f52a788c5cbc4a4
Hernany Motoso <contatohernany@gmail.com>
2022-04-17 16:43:55 -0300

    fix: fixed error on storybook config and add global font

--- packages/reactcomponents/src/components/Button/Button.tsx
+++ packages/reactcomponents/src/components/Button/Button.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Button as ButtonCSS } from 'styles';
-import { styled } from 'web';
+import { styled, globalCss } from 'web';
 
+const global = globalCss({
+  '@import': [
+    "url('https://fonts.googleapis.com/css2?family=Archivo&display=swap')",
+  ],
+  '@font-face': [
+    {
+      fontFamily: 'Archivo',
+      src: "url('https://fonts.googleapis.com/css2?family=Archivo&display=swap')",
+    },
+  ],
+});
 const StyledButton = styled('button', ButtonCSS);
 
 export type ButtonProps = React.ComponentProps<typeof StyledButton> & {
   t?: boolean;
 };
 
-export const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
-  <StyledButton {...props}>{children}</StyledButton>
-);
+export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  global();
+  return <StyledButton {...props}>{children}</StyledButton>;
+};
